1d35d26b3e24bdb2129f0444b5cae0193a92bee5
MrNithi <bossrocker777@gmail.com>
2021-07-31 11:45:42 +0700

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Tamagotchi from './components/Tamagotchi';
+
+const App = () => {
+  return <Tamagotchi />;
+};
+
+export default App;
---
+++ src/components/Tamagotchi.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tamagotchi = () => {
+  return <div className="text-pink-500">Tamagotchi</div>;
+};
+
+export default Tamagotchi;
